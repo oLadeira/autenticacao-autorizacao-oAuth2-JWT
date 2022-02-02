1de8c5b705f617fabfe10415c012e0d8a74837b4
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-02-02 10:19:16 -0300

    :tada: first commit

---
+++ src/main/java/com/lucasladeira/AutenticacaoAutorizacaoOAuth2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.lucasladeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoAutorizacaoOAuth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoAutorizacaoOAuth2JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasladeira/AutenticacaoAutorizacaoOAuth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasladeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoAutorizacaoOAuth2JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
